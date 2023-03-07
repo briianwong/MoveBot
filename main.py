c0bca4f7efb810502d097e11dab2cbe1417ad646
Brian Wong <bwong362@uwo.ca>
2023-03-07 16:22:02 -0500

    for darren

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import asyncio
 import discord
 from discord.ext import commands
+import tokenKey
 
-TOKEN = "MTA4MjQ4MTk2MTU4ODc2MDYzNw.G47SdT.oYRmBHig5M_xZdZf4C_XtaRqW5L5_C2rC2q94s"
+TOKEN = tokenKey.token
 
 intents = discord.Intents.all()
 
@@ -19,11 +19,13 @@ async def move (ctx, initialChannel, moveToChannel):
    # Get the channels
    # initial_channel = discord.utils.get(bot.channels, id=initial_channel_id)
    # move_to_channel = discord.utils.get(bot.channels, did=initial_channel_id)
-   
-   initialChannel = discord.utils.get(ctx.guild.voice_channels, name='office')
-   #initial_channel_id = channel.id
-   moveToChannel = discord.utils.get(ctx.guild.voice_channels, name='voice1')   
-   #move_to_channel_id = channel.id
+
+
+   initialChannel = discord.utils.get(ctx.guild.voice_channels, name = "office")
+   #initial_channel_id = discord.VoiceChannel.id
+   moveToChannel = discord.utils.get(ctx.guild.voice_channels, name = "voice1")   
+   #move_to_channel_id = discord.VoiceChannel.id
+    
 
 
    # Check if the channel types are valid
@@ -45,6 +47,9 @@ async def move (ctx, initialChannel, moveToChannel):
 
    await ctx.send(f'Moved all members from {initialChannel.mention} to {moveToChannel.mention}')
 
-print ("hello world")
+@bot.event
+async def on_ready():
+   print ("hello world")
+   print("ready")
 
 bot.run(TOKEN)
