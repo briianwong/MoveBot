5610fa4f0b86ca17d1bb3f70cbf6b4caf339c4f4
Brian Wong <bwong362@uwo.ca>
2023-03-08 01:18:10 -0500

    Co-authored-by: simenghe <simenghe@users.noreply.github.com>

--- main.py
+++ main.py
@@ -6,28 +6,21 @@ TOKEN = tokenKey.token
 
 intents = discord.Intents.all()
 
-#discord.Member = discord.utils.get(bot.member, id = member)
-
-# @bot.command(pass_context=True)
-# async def move(ctx, member: discord.Member, channel: discord.Channel):
-#     await bot.move_member(member, channel)
-
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+# This command moves the entire channel
 @bot.command()
-async def move (ctx, initialChannel, moveToChannel):
+async def movec (ctx, initialChannel, moveToChannel):
    # Get the channels
    initialChannel = discord.utils.get(ctx.guild.voice_channels, name = str(initialChannel))
    moveToChannel = discord.utils.get(ctx.guild.voice_channels, name = str(moveToChannel))   
-    
-
-
+   
    # Check if the channel types are valid
    if not isinstance(initialChannel, (discord.VoiceChannel)):
-      await ctx.send('Invalid ID for `initial_channel_id`')
+      await ctx.send('Invalid ID for `initialChannel`')
       return
    if not isinstance(moveToChannel, (discord.VoiceChannel)):
-      await ctx.send('Invalid ID for `move_to_channel_id`')
+      await ctx.send('Invalid ID for `moveToChannel`')
       return
 
    # Move all the members:
@@ -39,6 +32,35 @@ async def move (ctx, initialChannel, moveToChannel):
    
    await ctx.send(f'Moved all members from {initialChannel.mention} to {moveToChannel.mention}')
 
+
+# This command moves the specified users
+@bot.command()
+async def moveu (ctx, *args):
+   users = []
+   for i in args:
+      moveToChannel = None
+      if not i.startswith('<@'):
+         moveToChannel = discord.utils.get(ctx.guild.voice_channels, name = str(i))
+         print (moveToChannel)
+
+      if i.startswith('<@'):
+         #print(i) #this will print the id
+         usersMentioned = discord.utils.get(ctx.guild.members, id = int(i[2:-1]))
+         print(usersMentioned) #this prints the username
+         users.append(usersMentioned)
+         # await usersMentioned.move_to(moveToChannel)
+
+
+   if not moveToChannel:
+      await ctx.send(f'dumbass there is no destination channel cane!')
+      return
+    
+   for user in users:
+      await user.move_to(moveToChannel)
+   
+   await ctx.send(f'Moved all members to {moveToChannel.mention}')
+
+      
 @bot.event
 async def on_ready():
    print("ready")
